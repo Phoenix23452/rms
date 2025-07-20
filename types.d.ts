b42efa55f146f4c304a662b904616bf51def5301
Phoenix <1phoenix23452@gmail.com>
2025-07-20 15:51:02 +0500

    Adding Types

--- types.d.ts
+++ types.d.ts
@@ -8,3 +8,72 @@ type Category = {
     products: number;
   };
 };
+
+type Product = {
+  id: number;
+  name: string;
+  slug: string;
+  description?: string | null;
+  regularPrice: number;
+  discountPercentage?: number | null;
+  isFeatured: boolean;
+  isPopular: boolean;
+  status: boolean;
+  image?: string | null;
+  categoryId: number;
+  category?: Category;
+  variants?: Variant[];
+  dealItems?: DealItem[];
+  createdAt: string;
+  updatedAt: string;
+};
+
+type Variant = {
+  id: number;
+  name: string;
+  price: number;
+  productId: number;
+  product?: Product;
+};
+
+type Deal = {
+  id: number;
+  name: string;
+  regularPrice: number;
+  offerPrice?: number | null;
+  description?: string | null;
+  isFeatured: boolean;
+  image?: string | null;
+  status: boolean;
+
+  availableFrom?: string | null;
+  availableUntil?: string | null;
+  startDate?: string | null;
+  endDate?: string | null;
+  availableDays: Day[]; // Enum
+
+  dealItems?: DealItem[];
+
+  createdAt: string;
+  updatedAt: string;
+};
+
+type DealItem = {
+  id: number;
+  productId: number;
+  product?: Product;
+  quantity: number;
+  note?: string | null;
+  dealId: number;
+  deal?: Deal;
+};
+
+enum Day {
+  Mon = "Mon",
+  Tue = "Tue",
+  Wed = "Wed",
+  Thu = "Thu",
+  Fri = "Fri",
+  Sat = "Sat",
+  Sun = "Sun",
+}
